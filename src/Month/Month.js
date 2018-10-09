62eba7c21b1b9dafa86955ca8dae6509b08cffd8
Robbie Mackay <rm@robbiemackay.com>
2018-10-09 14:57:34 +1300

    refactor: Tidy up week building code a little

--- src/Month/Month.js
+++ src/Month/Month.js
@@ -4,26 +4,27 @@ import { format, getWeeksInMonth, getDaysInMonth, getDay } from 'date-fns';
 import './Month.css';
 import Day from '../Day/Day';
 
-class Month extends Component {
-
-  buildWeekArrays(firstOfMonth) {
-    let weeks = [...Array(getWeeksInMonth(firstOfMonth))].map((e, index) => [...Array(7)]);
-    let endDate = getDaysInMonth(firstOfMonth);
-    let week = 0;
-    for (let date = 1; date <= endDate; date++) {
-      let index = getDay(new Date(this.props.year, this.props.month, date));
-      weeks[week][index] = date;
-      if (index === 6) {
-        week++;
-      }
+const buildWeeksArray = (year, month) => {
+  let firstOfMonth = new Date(year, month, 1);
+  let weeks = [...Array(getWeeksInMonth(firstOfMonth))].map((e, index) => [...Array(7)]);
+  let endDate = getDaysInMonth(firstOfMonth);
+  let week = 0;
+  for (let date = 1; date <= endDate; date++) {
+    let index = getDay(new Date(year, month, date));
+    weeks[week][index] = date;
+    if (index === 6) {
+      week++;
     }
-
-    return weeks;
   }
 
+  return weeks;
+}
+
+class Month extends Component {
+
   render() {
-    let firstOfMonth = new Date(this.props.year, this.props.month, 1);
-    let weeks = this.buildWeekArrays(firstOfMonth);
+    let monthFormatted = format(new Date(this.props.year, this.props.month, 1), 'LLLL');
+    let weeks = buildWeeksArray(this.props.year, this.props.month);
 
     return (
       <div className="Month">
@@ -31,7 +32,7 @@ class Month extends Component {
           <thead>
             <tr>
               <th colSpan="7">
-                {format(firstOfMonth, 'LLLL')}
+                {monthFormatted}
               </th>
             </tr>
             <tr className="bg-light">
