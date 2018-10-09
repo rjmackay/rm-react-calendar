f9696e03a270d89d7a2429fdb48787648aea455d
Robbie Mackay <rm@robbiemackay.com>
2018-10-09 16:23:47 +1300

    feat: Add 'Jump to current year' and tidy up layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, Table } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Table } from 'reactstrap';
 import './App.css';
 import Calendar from './Calendar/Calendar';
 import Day from './Day/Day';
@@ -11,6 +11,11 @@ class App extends Component {
     this.state = { year: 2018 };
 
     this.handleChangeYear = this.handleChangeYear.bind(this);
+    this.handleJumpToCurrentYear = this.handleJumpToCurrentYear.bind(this);
+  }
+
+  handleJumpToCurrentYear(event) {
+    this.setState({year: (new Date()).getFullYear()});
   }
 
   handleChangeYear(event) {
@@ -28,26 +33,32 @@ class App extends Component {
         <main>
           <Calendar year={this.state.year}></Calendar>
 
-          <Table size="sm" className="col-4">
-            <thead>
-              <tr>
-                <th colSpan="7">
-                  Key
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              { Object.keys(categories).map((category) => <tr key={category}><Day date={category} category={category} /></tr>) }
-            </tbody>
-          </Table>
+          <div className="row">
+            <div className="col-4">
+              <Button onClick={this.handleJumpToCurrentYear}>Jump to current year</Button>
+              <Form>
+                <FormGroup>
+                  <Label>Change year: <Input type="number" value={this.state.year} onChange={this.handleChangeYear} /></Label>
+                </FormGroup>
+              </Form>
+            </div>
+            <div className="col-4"></div>
+            <div className="col-4">
+              <Table size="sm">
+                <thead>
+                  <tr>
+                    <th colSpan="7">
+                      Key
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { Object.keys(categories).map((category) => <tr key={category}><Day date={category} category={category} /></tr>) }
+                </tbody>
+              </Table>
+            </div>
+          </div>
         </main>
-        <footer>
-          <Form>
-            <FormGroup>
-              <Label>Change year: <Input type="number" value={this.state.year} onChange={this.handleChangeYear} /></Label>
-            </FormGroup>
-          </Form>
-        </footer>
       </div>
     );
   }
