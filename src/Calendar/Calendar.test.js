28735d91e63fca4eb4018222842902d4c7b8bdd2
Robbie Mackay <rm@robbiemackay.com>
2018-10-09 16:15:12 +1300

    tests: Add more simple tests for calendar and month

--- src/Calendar/Calendar.test.js
+++ src/Calendar/Calendar.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Calendar from './Calendar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Calendar year="2018" />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders 12 months', () => {
+  const calendar = shallow(<Calendar year="2018" />);
+  expect(calendar.find('Month')).toHaveLength(12);
+});
+
+it('renders the year', () => {
+  const calendar = shallow(<Calendar year="2020" />);
+  expect(calendar.text()).toContain(2020);
 });
--- src/Month/Month.test.js
+++ src/Month/Month.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Month from './Month';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  const tr = document.createElement('tr');
-  ReactDOM.render(<Month year="2018" month="10" />, tr);
-  ReactDOM.unmountComponentAtNode(tr);
+  const div = document.createElement('div');
+  ReactDOM.render(<Month year="2018" month="10" />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correct number of days', () => {
+  const month = shallow(<Month year="2018" month="9" />);
+   // Note this is 5 weeks * 7 days because it includes empty days in the grid
+  expect(month.find("Table").shallow().find('Day')).toHaveLength(35);
+
+  const month1 = shallow(<Month year="2017" month="4" />);
+  expect(month1.find("Table").shallow().find('Day')).toHaveLength(35);
+
+  const month2 = shallow(<Month year="2017" month="3" />);
+  expect(month2.find("Table").shallow().find('Day')).toHaveLength(42);
+});
+
+it('renders the year', () => {
+  const month = shallow(<Month year="2020" month="8" />);
+  expect(month.find("Table").shallow().text()).toContain("September");
 });
