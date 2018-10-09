a8fea6682eb4075b8818e8d32d46b16fd1afad33
Robbie Mackay <rm@robbiemackay.com>
2018-10-09 14:45:10 +1300

    feat: Add quick year changer to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Calendar from './Calendar/Calendar';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { year: 2018 };
+
+    this.handleChangeYear = this.handleChangeYear.bind(this);
+  }
+
+  handleChangeYear(event) {
+    this.setState({year: parseInt(event.target.value, 10)});
+  }
+
   render() {
     return (
       <div className="App container">
@@ -12,8 +24,15 @@ class App extends Component {
           </h2>
         </header>
         <main>
-          <Calendar year={2018}></Calendar>
+          <Calendar year={this.state.year}></Calendar>
         </main>
+        <footer>
+          <Form>
+            <FormGroup>
+              <Label>Change year: <Input type="number" value={this.state.year} onChange={this.handleChangeYear} /></Label>
+            </FormGroup>
+          </Form>
+        </footer>
       </div>
     );
   }
